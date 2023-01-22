ef996831aedfda20eb5b4c1fe49c963e6c7312b8
slanante1997 <>
2023-01-22 14:23:20 -0800

    Implemented Mixins and added more Tests

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -1,10 +1,24 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Logger.Tests
 {
     [TestClass]
     public class FileLoggerTests
     {
-        
+        [TestMethod]
+        public void FileLogger_SampleFileWriteAndRead()
+        {
+            // Arrange
+            string filePath = "C:\\Users\\Public\\Documents\\example.txt";
+            var logger = new FileLogger(filePath) {ClassName = nameof(FileLoggerTests)};
+
+            // Act
+            logger.Log(LogLevel.Information, "Here be some Info, yarr!");
+
+            // Assert
+            string[] content = File.ReadAllLines(filePath);
+            Assert.IsTrue(content[0].Contains("Here be some Info, yarr!"));
+        }
     }
 }
--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -1,7 +1,48 @@
-﻿namespace Logger
+﻿using System;
+
+namespace Logger
 {
     public static class BaseLoggerMixins
     {
-        
+        public static void Error(this BaseLogger logger, string message, params object[] arguments)
+        {
+            if (logger == null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
+
+            logger.Log(LogLevel.Error, string.Format(message, arguments));
+        }
+
+        public static void Warning(this BaseLogger logger, string message, params object[] arguments)
+        {
+            if (logger == null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
+
+            logger.Log(LogLevel.Warning, string.Format(message, arguments));
+        }
+
+
+        public static void Information(this BaseLogger logger, string message, params object[] arguments)
+        {
+            if (logger == null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
+
+            logger.Log(LogLevel.Information, string.Format(message, arguments));
+        }
+
+        public static void Debug(this BaseLogger logger, string message, params object[] arguments)
+        {
+            if (logger == null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
+
+            logger.Log(LogLevel.Debug, string.Format(message, arguments));
+        }
     }
 }
