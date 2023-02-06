b7d7bf506e06c1d57b514d85be1ad0b1c8e8e116
Josh <jpedersen@ewu.edu>
2023-02-05 18:40:52 -0800

    Tests Added Added more entity variation to StorageTests Added testing for FullName

---
+++ Logger.Tests/FullNameTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logger.Tests;
+
+[TestClass]
+public class FullNameTests
+{
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void FullName_NullFirst_NullException()
+    {
+        // Arrange
+
+        string? name = null;
+
+        // Act
+        
+        Employee entity = new()
+        {
+            Id = Guid.NewGuid(),
+            FullName = new FullName(name!, "Stevens"),
+            Employer = "Random Name"
+        }; 
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void FullName_NullLast_NullException()
+    {
+        // Arrange
+
+        string? name = null;
+
+        // Act
+
+        Employee entity = new()
+        {
+            Id = Guid.NewGuid(),
+            FullName = new FullName("Jeff", name!),
+            Employer = "Different Name"
+        };
+    }
+
+    [TestMethod]
+    public void FullName_Creation_ReturnsName()
+    {
+        // Arrange
+
+        string name = "John Green Smith";
+
+        // Act
+
+        Student student = new()
+        {
+            Id = Guid.NewGuid(),
+            FullName = new FullName("John", "Smith", "Green"),
+            StudentYear = StudentLevels.Freshman
+        };
+
+        // Assert
+
+        Assert.AreEqual(name, student.FullName.Name);
+    }
+}
--- Logger.Tests/StorageTests.cs
+++ Logger.Tests/StorageTests.cs
@@ -6,7 +6,7 @@ namespace Logger.Tests;
 public class StorageTests
 {
     [TestMethod]
-    public void Adding_Entities()
+    public void Adding_Entities_Employee()
     {
         Storage storage = new Storage();
         Employee entity = new Employee
@@ -21,5 +21,75 @@ public class StorageTests
 
         // Assert
         Assert.IsTrue(storage.Contains(entity));
+    }
+
+    [TestMethod]
+    public void Adding_Entities_Student()
+    {
+        // Arrange
+
+        Storage storage = new();
+        Student entity = new Student
+        {
+            Id = Guid.NewGuid(),
+            FullName = new FullName("Foo", "Bar"),
+            StudentYear = StudentLevels.Junior
+        };
+
+        // Act
+
+        storage.Add(entity);
+
+        // Assert
+
+        Assert.IsTrue(storage.Contains(entity));
+    }
+
+    [TestMethod]
+    public void Adding_Entities_Book()
+    {
+        // Arrange
+
+        FullName author = new FullName("John", "Snow");
+
+        Storage storage = new();
+        Book entity = new Book
+        {
+            Id = Guid.NewGuid(),
+            BookName = "Something About Game of Thrones",
+            Author = author,
+            ISBN = "9781004367239"
+        };
+
+        // Act
+
+        storage.Add(entity);
+
+        // Assert
+
+        Assert.IsTrue(storage.Contains(entity));
+    }
+
+    [TestMethod]
+    public void Removing_Entities_General()
+    {
+        // Arrange
+
+        Storage storage = new();
+        Student entity = new Student
+        {
+            Id = Guid.NewGuid(),
+            FullName = new FullName("Rand", "Person"),
+            StudentYear = StudentLevels.Senior
+        };
+
+        // Act
+
+        storage.Add(entity);
+        storage.Remove(entity);
+
+        // Assert
+
+        Assert.IsFalse(storage.Contains(entity));
     }
 }
