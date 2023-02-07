29136cd96162b80ad7f91febf5e8ef16a1c8a5e3
slanante <103216077+slanante@users.noreply.github.com>
2023-02-07 15:18:50 -0800

    Update Node.cs
    Added Append, Exists and Clear methods

--- GenericsHomework/Node.cs
+++ GenericsHomework/Node.cs
@@ -2,8 +2,8 @@ namespace GenericsHomework;
 public class Node<T>
 {
     public T Content { get; set; }
-    public Node NextNode { get; set; }
-    public Node? PrevNode { get; set; }
+    private Node NextNode { get; set; }
+    private Node? PrevNode { get; set; }
 
     public Node(T content)
     {
@@ -14,5 +14,30 @@ public class Node<T>
     {
         return Content.ToString();
     }
+
+    public void Append(T content)
+    {
+        NextNode = new Node<T>(content);
+    }
+
+    public void Clear()
+    {
+        NextNode = null;
+        PrevNode = null;
+    }
+    public bool Exists(T value)
+    {
+        Node currNode = this;
+        while (currNode != null)
+        {
+            if (currNode.Content.Equals(value))
+            {
+                return true;
+            }
+            currNode = currNode.NextNode;
+        }
+        return false;
+    }
+    
 
 }
