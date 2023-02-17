7b66f8842feffdf8f2d9d168a01fcae6b4435a04
lsobtafo <69612582+lsobtafo@users.noreply.github.com>
2023-02-16 16:44:16 -0800

    Added Extra Credit (Venn Diagram and Circle)

---
+++ GenericsHomeWork.Tests/VennDiagramTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using GenericsHomework;
+
+namespace NodeTests
+{
+	public class VennDiagramTests
+	{
+        [TestMethod]
+        public void OutputTheNameOfVennDiag_Correct()
+        {
+            VennDiagram<Circle<string>> vennDiag = new VennDiagram<Circle<string>>();
+
+            string actualName = vennDiag.Name;
+
+            Assert.AreEqual("Venn Diagram", actualName);
+        }
+
+        [TestMethod]
+        public void ToString_ReturnsCorrectValue_WhenCirclesNotEmpty()
+        {
+            // Arrange
+            var circles = new List<int> { 1, 2, 3 };
+            var vennDiagram = new VennDiagram<int>("My Venn Diagram") { Circles = circles };
+
+            // Act
+            var actualResult = vennDiagram.ToString();
+
+            // Assert
+            var expectedResult = "1 2 3 ";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+
+        [TestMethod]
+        public void VerifyItemsOfCircle_Correct()
+        {
+            Circle<string> circle = new Circle<string>("Birds", new string[] { "Penguin", "Chicken", "Hummingbird" });
+
+            string result = circle.ToString();
+            string answer = "Penguin Chicken Hummingbird ";
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result, answer);
+        }
+
+        [TestMethod]
+        public void VerifyVennIntersection_Correct()
+        {
+            Circle<string> circle = new Circle<string>("Birds", new string[] { "Penguin", "Chicken", "Hummingbird" });
+            Circle<string> circle2 = new Circle<string>("Thanksgiving Food", new string[] { "Turkey", "Chicken", "Mashed Potatoes" });
+            string[] intersect = circle.Intersect(circle2);
+            Assert.AreEqual(intersect[0], "Chicken");
+        }
+    }
+}
+
--- GenericsHomework/VennDiagram.cs
+++ GenericsHomework/VennDiagram.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 namespace GenericsHomework
 {
-	public class VennDiagram
-	{
-		public VennDiagram()
-		{
-		}
-	}
+	public class VennDiagram<T>
+    {
+        public List<T> Circles { get; set; } = new();
+        public string Name { get; set; }
+
+        public VennDiagram(string name = null!)
+        {
+            Name = name ?? "Venn Diagram";
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            foreach (T element in Circles)
+            {
+                result += element!.ToString() + " ";
+            }
+            return result;
+        }
+    }
 }
 
